fa597afdc3bdf8d31b834aef085ddae5ec4e02ed
KiddFP1 <mr.dang112310@gmail.com>
2024-04-27 02:20:06 +0700

    Them file thi thu java3

---
+++ src/main/java/com/poly/entity/MyConstants.java
@@ -0,0 +1,9 @@
+package com.poly.entity;
+
+public class MyConstants {
+	// Replace with your email here:  
+	   public static final String MY_EMAIL = "mr.dang112310";
+	   
+	    // Replace password!!
+	    public static final String MY_PASSWORD = "oqij tmho ymzk tgev";
+}
