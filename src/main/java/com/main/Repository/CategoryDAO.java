da45e27df9ec20d4ff6548dc1dbd490f7c0f3418
KiddFP1 <mr.dang112310@gmail.com>
2024-06-04 12:58:11 +0700

    Delete all contents

--- src/main/java/com/main/Repository/CategoryDAO.java
+++
@@ -1,8 +0,0 @@
-package com.main.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.main.Entity.Category;
-
-public interface CategoryDAO extends JpaRepository<Category, String> {
-}
